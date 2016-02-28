fdc9038dcfbd58702995afa94fe3057e12904575
silchi <silchi72@gmail.com>
2016-02-28 15:29:50 +0330

    added forecast

--- src/model/Country.java
+++ src/model/Country.java
@@ -13,6 +13,8 @@ import java.util.List;
  * @author amir
  */
 public class Country {
+	
+	public static final String MY_FORECAST = "MY_FORECAST";
 	public int code;
     public String countryName;
     public ArrayList<GrowthRate> growths = new ArrayList<GrowthRate>();
@@ -44,9 +46,47 @@ public class Country {
     public List<PredictPopulation> getPredictForMenWomenOrAll(String methodName, int type){
     	if (type != 2){
     		List<PredictPopulation> ret = new ArrayList<PredictPopulation>();
-    		for (PredictPopulation predict: predicts.get(type))
-    			if (predict.methodName.equals(methodName))
-    				ret.add(predict);
+    		if (methodName.equals(MY_FORECAST)){
+    			List<PredictPopulation> est = getPredictForMenWomenOrAll(ModelMgr.ESTIMATES, type);
+    			final int startYear = 1950;
+    			final int endYear = 2015;
+    			double[][] f = new double[endYear-startYear+1][endYear-startYear+1];
+    			for (PredictPopulation e: est)
+    				f[e.predictYear-startYear][0] = e.populationNumber;
+    			for (int j=1; j<=endYear-startYear; j++)
+    				for (int i=0; i<=endYear-startYear-j; i++)
+    					f[i][j] = f[i+1][j-1]-f[i][j-1];
+    			
+    			for (int i=0; i<10; i++){
+    				for (int j=0; j<10; j++)
+    					System.err.print(f[i][j] + " ");
+    				System.err.println();
+    			}
+    			final int predictStartYear = 2015;
+    			final int predictEndYear = 2100;
+    			for (int i=predictStartYear; i<=predictEndYear; i++){
+    				double r = i-endYear;
+    				PredictPopulation pp = new PredictPopulation();
+    				pp.methodName = methodName;
+    				pp.predictYear = i;
+    				double fx = 0.0;
+    				double rcoeff = 1.0;
+    				for (int j=0; j<=Math.min(4, endYear-startYear); j++){
+    					if (j>0){
+    						rcoeff*= r+j-1;
+    						rcoeff/=j;
+    					}
+    					fx+= rcoeff * f[endYear-startYear-j][j];
+    				}
+    				pp.populationNumber = (int)Math.round(fx);
+    				ret.add(pp);
+    				System.err.println(i + " " + fx);
+    			}
+    		}else{
+	    		for (PredictPopulation predict: predicts.get(type))
+	    			if (predict.methodName.equals(methodName))
+	    				ret.add(predict);
+    		}
     		return ret;
     	}else{
     		List<PredictPopulation> males = getPredictForMenWomenOrAll(methodName, 0);
